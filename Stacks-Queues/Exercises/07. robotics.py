49984af7b988b589e34e47c8d13cc3461c162f3f
geodimitrov <geo.dimitrov@yahoo.com>
2021-03-17 18:24:18 +0200

    Adding solutions

---
+++ Stacks-Queues/Exercises/07. robotics.py
@@ -0,0 +1,29 @@
+# import datetime
+#
+# def read_robot_data():
+#     data = [data.split("-") for data in input().split(";")]
+#     return {robot[0]: int(robot[1]) for robot in data}
+#
+# def read_start_time():
+#     return input()
+#
+# def process_products():
+#
+#     while True:
+#         product = input()
+#         if product == "End":
+#             break
+#
+#
+# robots = read_robot_data()
+# start_time = read_start_time()
+# # process_products()
+# print(robots)
+# current_time = datetime.time(11, 8, seconds)
+#
+# seconds = "400"
+# print(str.zfill(seconds, 5))
+
+
+
+
--- Tuples-Sets/Exercises/03. periodic_table.py
+++ Tuples-Sets/Exercises/03. periodic_table.py
@@ -1,14 +1,31 @@
-def create_set_elements(count):
-    elements = []
-    for _ in range(count):
+# Solution 1
+def read_lines(n):
+    res = []
+    for _ in range(n):
         for el in input().split():
-            elements.append(el)
-    return set(elements)
+            res.append(el)
+    return res
 
-def print_result(set):
-    for el in set:
-        print(el)
+def read_input():
+    n = int(input())  # num of lines
+    result = read_lines(n)
+    return result
+
+def print_result(elements):
+    print("\n".join(set(elements)))
+
+elements = read_input()
+print_result(elements)
+
+
+# Solution 2
 
 n = int(input())
-elements = create_set_elements(n)
-print_result(elements)
+elements = set()
+
+for _ in range(n):
+    line = input().split()
+    for el in line:
+        elements.add(el)
+
+print("\n".join(elements))
