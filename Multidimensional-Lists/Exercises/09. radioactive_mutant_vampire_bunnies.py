98a95ce900916b7f5280fdc5250c62e19bbc8834
geodimitrov <geo.dimitrov@yahoo.com>
2021-04-28 21:05:43 +0300

    Adding solutions

--- Multidimensional-Lists/Exercises/09. radioactive_mutant_vampire_bunnies.py
+++ Multidimensional-Lists/Exercises/09. radioactive_mutant_vampire_bunnies.py
@@ -1,87 +1,124 @@
-def create_matrix(rows):
-    result = []
-    for row in rows:
-        result.append([el for el in row])
-    return result
+player = "P"
+bunny = "B"
+dot = "."
 
-def player(matrix, row, col):
-    if matrix[row][col] == "P":
-        return True
+# matrix_data = [
+#     ".......B",
+#     "...B....",
+#     "....B..B",
+#     "........",
+#     "..P.....",
+# ]
 
-def find_player_position(matrix, rows, columns):
-    for row in range(rows):
-        for col in range(columns):
-            if player(matrix, row, col):
-                return row, col
-
-def row_not_in_range(row, rows):
-    if row not in range(rows):
-        return True
-
-def column_not_in_range(col, columns):
-    if col not in range(columns):
-        return True
-
-def read_command(command, row, col, rows, columns):
-    won_game = False
-
-    if command == "U":
-        if row_not_in_range(row - 1, rows):
-            won_game = True
-        else:
-            row -= 1
-
-    elif command == "D":
-        if row_not_in_range(row + 1, rows):
-            won_game = True
-        else:
-            row += 1
-
-    elif command == "L":
-        if column_not_in_range(col - 1, columns):
-            won_game = True
-        else:
-            col -= 1
-
-    else:
-        if column_not_in_range(col + 1, columns):
-            won_game = True
-        else:
-            col += 1
-
-    return row, col, won_game
-
-
-def print_won_game(matrix, row, col):
-    for line in matrix:
-        print("".join(line))
-    print(f"won: {row} {col}")
-
-def move_player(matrix, row, col):
-    pass
-
-
-def play_game(commands, matrix, player_position, rows, columns):
-    row, col = player_position
-
-    for command in commands:
-        row, col, won_game = read_command(command, row, col, rows, columns)
-        if won_game:
-            return print_won_game(matrix, row, col)
-        move_player()
-
-
-rows, columns = [int(el) for el in "5 8".split()]
-
-matrix_data = [
-    ".......B",
-    "...B....",
-    "....B..B",
-    "........",
-    "..P.....",
+deltas = [
+    (0, -1), (0, +1),
+    (-1, 0), (+1, 0)
 ]
 
-matrix = create_matrix(matrix_data)
-commands = [el for el in "ULLL"]
-player_position = find_player_position(matrix, rows, columns)
-play_game(commands, matrix, player_position, rows, columns)
+def create_matrix(rows):
+    result = []
+
+    for r in range(rows):
+        line = input()
+        result.append([char for char in line])
+
+    return result
+
+def get_players_position(matrix):
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == player:
+                return row, col
+
+
+def get_next_move_dimensions(command, curr_row, curr_col):
+
+    if command == "L":
+        curr_col -= 1
+
+    elif command == "R":
+        curr_col += 1
+
+    elif command == "U":
+        curr_row -=1
+
+    else:
+        curr_row += 1
+
+    return curr_row, curr_col
+
+
+def inside_matrix(row, column, rows, columns):
+    return row in range(rows) and column in range(columns)
+
+
+def get_bunnies_positions(matrix, rows, columns):
+    result = []
+
+    for row in range(rows):
+        for col in range(columns):
+            if matrix[row][col] == bunny:
+                result.append((row, col))
+
+    return result
+
+
+def multiply_bunnies(matrix, rows, columns):
+    bunnies_to_multiply = get_bunnies_positions(matrix, rows, columns)
+    global is_dead
+
+    for position in bunnies_to_multiply:
+        curr_row, curr_col = position
+
+        for delta in deltas:
+            next_row = curr_row+ delta[0]
+            next_col = curr_col + delta[1]
+
+            if inside_matrix(next_row, next_col, rows, columns):
+                if matrix[next_row][next_col] == player:
+                    is_dead = True
+                matrix[next_row][next_col] = bunny
+
+
+def print_result(matrix, is_won, position):
+    print("\n".join(''.join(row) for row in matrix))
+    position_str = ' '.join(map(str, position))
+
+    if is_won:
+        print(f"won: {position_str}")
+    else:
+        print(f"dead: {position_str}")
+
+
+rows, columns = map(int, input().split())
+matrix = create_matrix(rows)
+commands = input()
+players_position = get_players_position(matrix)
+is_won = False
+is_dead = False
+
+for command in commands:
+
+    if is_won or is_dead:
+        break
+
+    curr_row, curr_col = players_position
+    next_row, next_col = get_next_move_dimensions(command, curr_row, curr_col)
+
+    if not inside_matrix(next_row, next_col, rows, columns):
+        matrix[curr_row][curr_col] = dot
+        is_won = True
+
+    else:
+        if matrix[next_row][next_col] == bunny:
+            is_dead = True
+        else:
+            matrix[curr_row][curr_col] = dot
+            matrix[next_row][next_col] = player
+
+        players_position = next_row, next_col
+
+    multiply_bunnies(matrix, rows, columns)
+
+print_result(matrix, is_won, players_position)
--- Stacks-Queues/Exercises/07. robotics.py
+++ Stacks-Queues/Exercises/07. robotics.py
@@ -1,29 +1,71 @@
-# import datetime
-#
-# def read_robot_data():
-#     data = [data.split("-") for data in input().split(";")]
-#     return {robot[0]: int(robot[1]) for robot in data}
-#
-# def read_start_time():
-#     return input()
-#
-# def process_products():
-#
-#     while True:
-#         product = input()
-#         if product == "End":
-#             break
-#
-#
-# robots = read_robot_data()
-# start_time = read_start_time()
-# # process_products()
-# print(robots)
-# current_time = datetime.time(11, 8, seconds)
-#
-# seconds = "400"
-# print(str.zfill(seconds, 5))
+from datetime import time, datetime, timedelta
+
+END_COMMAND = "End"
+
+def read_product_lines():
+    result = []
+
+    while True:
+        command = input()
+
+        if command == END_COMMAND:
+            break
+
+        result.append(command)
+
+    return result
+
+def read_input():
+    robots_data = [el.split("-") for el in input().split(";")]
+    starting_time = input()
+    products = read_product_lines()
+
+    return robots_data, starting_time, products
 
 
+def create_robots_from_data(data):
+    return {el[0]: [int(el[1]), int(el[1])] for el in data}
 
 
+def format_starting_time(time):
+    hours, minutes, seconds = time.split(":")
+    result = datetime(100, 1, 1, int(hours), int(minutes), int(seconds))
+
+    return result
+
+def is_available(robots, robot):
+    processing_capacity = robots[robot][0]
+    passed_processing_time = robots[robot][0]
+    return processing_capacity == passed_processing_time
+
+
+def print_details(product, robot_name, time):
+    print(f"{robot_name} - {product} [{time.time()}]")
+
+
+def process_products(products, robots, start_time):
+    robots_line = [robot for robot in robots]
+
+    while products:
+
+        start_time += timedelta(0, 1)
+        processing_robot = robots_line[0]
+
+        if is_available(robots, processing_robot):
+            product = products.pop(0)
+            robots_line.append(robots_line.pop(0))
+            print_details(product, processing_robot, start_time)
+
+robots_data, starting_time, products = read_input()
+robots = create_robots_from_data(robots_data)
+formatted_start_time = format_starting_time(starting_time)
+process_products(products, robots, formatted_start_time)
+
+
+'''
+import datetime
+a = datetime.datetime(100,1,1,11,34,59)
+b = a + datetime.timedelta(0,3) # days, seconds, then other fields.
+print(a.time())
+print(b.time())
+'''
