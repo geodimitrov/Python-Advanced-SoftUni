6d8b9c320732c0e8de559aa374e0a2417098ed5b
geodimitrov <geo.dimitrov@yahoo.com>
2021-05-10 11:52:09 +0300

    Added solution for problem 06

--- Multidimensional-Lists/Exercises/06. knight_game.py
+++ Multidimensional-Lists/Exercises/06. knight_game.py
@@ -1,5 +1,5 @@
 
-# test_board = [
+# board = [
 #     ["0", "K", "0", "K", "0"],
 #     ["K", "0", "0", "0", "K"],
 #     ["0", "0", "K", "0", "0"],
@@ -7,7 +7,6 @@
 #     ["0", "K", "0", "K", "0"]
 # ]
 
-knight = "K"
 k_dimensions = [
     (+1, -2), (-1, -2), (-2, -1), (-2, +1),
     (-1, +2), (+1, +2), (+2, +1), (+2, -1)
@@ -16,48 +15,51 @@ k_dimensions = [
 def create_board(size):
     result = []
     for _ in range(size):
-        line = [el for el in input()]
+        line = list(input())
         result.append(line)
 
     return result
 
 
-def get_knights_positions(board, size):
-    result = []
+def is_inside_board(row, column, size):
+    return 0 <= row < size and 0 <= column < size
 
-    for row in range(size):
-        for col in range(size):
-            if board[row][col] == knight:
-                result.append((row, col))
-
-    return result
-
-
-def is_inside_board(board, row, column):
-    return row in range(len(board)) and column in range(len(board))
-
-
-def move_knight_on_board(board, row, column, dimensions):
-    global counter
+def calc_kills(board, size, row, column, dimensions):
+    result = 0
 
     for dimension in dimensions:
         current_row = row + dimension[0]
         current_col = column + dimension[1]
 
+        if is_inside_board(current_row, current_col, size) \
+                and board[current_row][current_col] == "K":
+            result += 1
 
-        if is_inside_board(board, current_row, current_col) \
-                and board[current_row][current_col] == knight:
-            board[current_row][current_col] = "0"
-            counter += 1
+    return result
 
 size = int(input())
 board = create_board(size)
-knights = get_knights_positions(board, size)
-counter = 0
+min_killed_knights = 0
 
-for knight in knights:
-    row, col = knight
-    move_knight_on_board(board, row, col, k_dimensions)
+while True:
+    max_kills = 0
+    killer_knight_position = None
 
-print(board)
-print(counter)
+    for row in range(size):
+        for col in range(size):
+            if board[row][col] == "K":
+                kills = calc_kills(board, size, row, col, k_dimensions)
+
+                if max_kills < kills:
+                    max_kills = kills
+                    killer_knight_position = (row, col)
+
+    if killer_knight_position:
+        row, col = killer_knight_position
+        board[row][col] = "0"
+        min_killed_knights += 1
+
+    else:
+        break
+
+print(min_killed_knights)
